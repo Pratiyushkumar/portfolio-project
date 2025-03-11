94a59289453502df1f413e9d30ed0e27c0046973
Pratiyush Kumar <pratiyush.inv@gmail.com>
2025-03-11 20:53:56 +0530

    Navbar feature

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,8 +1,10 @@
 import { Outlet } from 'react-router';
+import Navbar from '../Navbar/Navbar';
 
 const Layout = () => {
   return (
     <div className="min-h-screen w-full bg-background text-foreground overflow-hidden">
+      <Navbar />
       <main className="h-full">
         <Outlet />
       </main>
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,5 @@
 const Home = () => {
-  return <div className="text-white">Home</div>;
+  return <div className="text-white"></div>;
 };
 
 export default Home;
